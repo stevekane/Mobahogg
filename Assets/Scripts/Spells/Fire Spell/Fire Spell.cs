8a92f147f99e11d4894b768421253b08b725cf75
lintrinwin <94869849+lintrinwin@users.noreply.github.com>
2024-12-18 11:24:58 -0600

    Tweaking Fireball

--- Assets/Scripts/Spells/Fire Spell/Fire Spell.cs
+++ Assets/Scripts/Spells/Fire Spell/Fire Spell.cs
@@ -1,14 +1,17 @@
 using System.Collections.Generic;
 using System.Threading;
 using Cysharp.Threading.Tasks;
+using Cysharp.Threading.Tasks.Triggers;
 using UnityEngine;
 
 public class FireSpell : Spell {
   [SerializeField] LocalClock LocalClock;
   [SerializeField] GameObject FireballPrefab;
+    [SerializeField] GameObject ExplosionPrefab;
   // TODO: Move to FireSpellSettings
   [SerializeField] float SpreadAngle = 60;
-  [SerializeField] float FireballSpeed = 20;
+  [SerializeField] float TravelSpeed = 20;
+    [SerializeField] float FanoutSpeed = 20;
   [SerializeField] int Count = 5;
   [SerializeField] int FanoutFrames = 5;
   [SerializeField] int TravelFrames = 60 * 5;
@@ -26,7 +29,7 @@ public class FireSpell : Spell {
     var right = Quaternion.LookRotation(Quaternion.Euler(0, SpreadAngle / 2, 0) * forward);
     for (var i = 0; i < Count; i++) {
       var orientation = Quaternion.Slerp(left, right, (float)i/(Count-1));
-      var fireball = Instantiate(FireballPrefab, position, orientation, transform);
+      var fireball = Instantiate(FireballPrefab, position, rotation, transform);
       Fireballs.Add(fireball);
       Orientations.Add(orientation);
     }
@@ -35,20 +38,21 @@ public class FireSpell : Spell {
         var fireball = Fireballs[i];
         var orientation = Orientations[i];
         var interpolant = (float)f/(FanoutFrames-1);
-        var easedInterpolant = EasingFunctions.EaseInQuint(interpolant);
-        var nextRotation = Quaternion.Slerp(orientation, rotation, easedInterpolant);
-        var nextPosition = fireball.transform.position + LocalClock.DeltaTime() * FireballSpeed * (nextRotation * Vector3.forward);
+        var easedInterpolant = EasingFunctions.EaseInQuad(interpolant);
+        var nextRotation = Quaternion.Slerp(rotation, orientation, easedInterpolant);
+        var nextPosition = fireball.transform.position + LocalClock.DeltaTime() * FanoutSpeed * (nextRotation * Vector3.forward);
         fireball.transform.SetPositionAndRotation(nextPosition, nextRotation);
       }
       await UniTask.Yield(PlayerLoopTiming.FixedUpdate, token);
     }
     for (var i = 0; i < Count; i++) {
+            Instantiate(ExplosionPrefab, Fireballs[i].transform.position, Fireballs[i].transform.rotation, transform);
       Fireballs[i].GetComponent<TrailRenderer>().enabled = true;
     }
     for (var f = 0; f < TravelFrames; f += LocalClock.DeltaFrames()) {
       for (var i = 0; i < Count; i++) {
         var fireball = Fireballs[i];
-        fireball.transform.position = fireball.transform.position + LocalClock.DeltaTime() * 1.5f *FireballSpeed * fireball.transform.forward;
+        fireball.transform.position = fireball.transform.position + LocalClock.DeltaTime() * TravelSpeed * fireball.transform.forward;
       }
       await UniTask.Yield(PlayerLoopTiming.FixedUpdate, token);
     }
