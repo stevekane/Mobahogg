46498dd2d099d25a69d643bba21742ac11d06f63
Steve Kane <kanesteven@gmail.com>
2025-01-02 19:50:06 -0600

    What the fuck?

--- Assets/Scripts/Jump Pad/Jump Pad.cs
+++ Assets/Scripts/Jump Pad/Jump Pad.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class JumpPad : MonoBehaviour {
   [SerializeField] JumpPadSettings Settings;
+  [SerializeField] UnityEvent OnTrigger;
 
   void OnTriggerEnter(Collider c) {
     if (c.TryGetComponent(out Player player) && c.TryGetComponent(out KCharacterController controller)) {
       controller.ForceUnground.Set(true);
       controller.Velocity.SetY(Settings.JumpStrength);
+      OnTrigger?.Invoke();
     }
   }
 }
