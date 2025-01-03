be26d4bec2a8c076be32d40091861623010b4738
Steve Kane <kanesteven@gmail.com>
2025-01-03 02:08:44 -0600

    Basic weapon hand IK added to stabilize motion during locomotion.

--- Assets/Scripts/Abilities/MoveAbility.cs
+++ Assets/Scripts/Abilities/MoveAbility.cs
@@ -6,9 +6,30 @@ public class MoveAbility : MonoBehaviour, IAbility<Vector2> {
   [SerializeField] Player Player;
   [SerializeField] MoveSpeed MoveSpeed;
   [SerializeField] LocalClock LocalClock;
+  [SerializeField] Vector3 HandRotationEulerAngles = Vector3.zero;
+  [SerializeField] bool UseConstraint;
 
   [Header("Writes To")]
   [SerializeField] KCharacterController CharacterController;
+  [SerializeField] AnimatorCallbackHandler AnimatorCallbackHandler;
+  [SerializeField] Animator Animator;
+
+  void Start() {
+    AnimatorCallbackHandler.OnIK.Listen(OnAnimatorIK);
+  }
+
+  void OnDestroy() {
+    AnimatorCallbackHandler.OnIK.Unlisten(OnAnimatorIK);
+  }
+
+  void OnAnimatorIK(int layer) {
+    var target = Player.transform.position + Vector3.up + 2 * Player.transform.right;
+    AnimatorCallbackHandler.Animator.SetIKPosition(AvatarIKGoal.RightHand, target);
+    // AnimatorCallbackHandler.Animator.SetIKRotation(AvatarIKGoal.RightHand, Quaternion.Euler(HandRotationEulerAngles) * Player.transform.rotation);
+    AnimatorCallbackHandler.Animator.SetIKRotation(AvatarIKGoal.RightHand, Quaternion.LookRotation(Player.transform.right));
+    AnimatorCallbackHandler.Animator.SetIKPositionWeight(AvatarIKGoal.RightHand, UseConstraint && CanRun ? 1 : 0);
+    AnimatorCallbackHandler.Animator.SetIKRotationWeight(AvatarIKGoal.RightHand, UseConstraint && CanRun ? 1 : 0);
+  }
 
   public bool CanRun
     => !LocalClock.Frozen()
@@ -18,7 +39,9 @@ public class MoveAbility : MonoBehaviour, IAbility<Vector2> {
 
   public bool TryRun(Vector2 value) {
     if (CanRun) {
-      CharacterController.DirectVelocity.Add(MoveSpeed.Value * value.XZ());
+      var velocity = MoveSpeed.Value * value.XZ();
+      CharacterController.DirectVelocity.Add(velocity);
+      Animator.SetFloat("Speed", velocity.magnitude);
       return true;
     } else {
       return false;
