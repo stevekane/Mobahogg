bc1c0e124dc0427a4a397baaa370a70625842065
Steve Kane <kanesteven@gmail.com>
2025-01-17 16:44:42 -0600

    Huge House-cleaning commit preparing the way for some changes. God help us all.

--- Assets/Core/Camera/PlayerVirtualCamera.cs
+++
@@ -1,9 +0,0 @@
-//using Unity.Cinemachine;
-//using UnityEngine;
-
-//public class PlayerVirtualCamera : MonoBehaviour {
-//  public static CinemachineVirtualCamera Instance;
-
-//  void Awake() => Instance = GetComponent<CinemachineVirtualCamera>();
-//  void OnDestroy() => Instance = null;
-//}
