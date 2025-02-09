e4cd1ea695bfe714e8828d5ebb5c43d694d95d14
Steve Kane <kanesteven@gmail.com>
2025-02-09 13:53:48 -0600

    Added support for directional hurt flinches to Player.

--- Assets/Scripts/Libs/Combat/Combatant.cs
+++ Assets/Scripts/Libs/Combat/Combatant.cs
@@ -3,8 +3,17 @@ using State;
 using UnityEngine;
 using UnityEngine.Events;
 
+// Expressed from victom's POV
+public enum AttackDirection {
+  Front = 0,
+  Right,
+  Back,
+  Left
+}
+
 public class Combatant : MonoBehaviour {
   [SerializeField] string HurtFlinchName = "Hurt Flinch";
+  [SerializeField] string HurtDirectionName = "Hurt Direction";
   [SerializeField] Animator Animator;
   [SerializeField] Health Health;
   [SerializeField] HitStop HitStop;
@@ -13,6 +22,18 @@ public class Combatant : MonoBehaviour {
   [SerializeField] Vibrator Vibrator;
   [SerializeField] UnityEvent<MeleeAttackEvent> OnHurt;
   [SerializeField] UnityEvent<MeleeAttackEvent> OnHit;
+
+  public float ToInterpolant(AttackDirection direction) => (int)direction / (float)3;
+
+  public AttackDirection AttackedFrom(Vector3 attackerForward, Vector3 victimForward) {
+    float angle = Vector3.SignedAngle(-victimForward, attackerForward, Vector3.up);
+    return angle switch {
+        >= -45f and <= 45f => AttackDirection.Front,
+        > 45f and <= 135f => AttackDirection.Right,
+        < -45f and >= -135f => AttackDirection.Left,
+        _ => AttackDirection.Back
+    };
+  }
 
   public Combatant LastAttacker { get; private set; }
 
@@ -25,8 +46,13 @@ public class Combatant : MonoBehaviour {
 
   public void Hurt(MeleeAttackEvent meleeAttackEvent) {
     LastAttacker = meleeAttackEvent.Attacker;
-    if (Animator && HurtFlinchName != "")
+    if (Animator && HurtFlinchName != "") {
+      var direction = AttackedFrom(meleeAttackEvent.Attacker.transform.forward, meleeAttackEvent.Victim.transform.forward);
+      var interpolant = ToInterpolant(direction);
+      Debug.Log($"{direction} {interpolant}");
+      Animator.SetFloat(HurtDirectionName, interpolant);
       Animator.SetTrigger(HurtFlinchName);
+    }
     if (Health)
       Health.Change(-meleeAttackEvent.Damage);
     if (HitStop)
